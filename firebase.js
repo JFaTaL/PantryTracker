0b0d7f0b80d13bd3da381ba6f98202b9147a1bd8
james <jamesjulian974138@gmail.com>
2024-08-07 20:07:17 -0400

    docs: small doc changes

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjcULqfstnfQ-lZD5p3ltM0oe1HJUCqwg",
-  authDomain: "kitchenhub-e8ad6.firebaseapp.com",
-  projectId: "kitchenhub-e8ad6",
-  storageBucket: "kitchenhub-e8ad6.appspot.com",
-  messagingSenderId: "679357493338",
-  appId: "1:679357493338:web:5af7a90089c3931bcca863",
-  measurementId: "G-R27D169NHG"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
